5deb37880b0a4c80e44a0cc07d3a68a8d2833299
bckang <kkang@presagia>
2022-03-01 10:13:18 -0500

    Initialize tailwindcss, Accessibility tool(axe-core)

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
+import './css/app.css';
 import App from './App';
-import { store } from './app/store';
-
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+const render = () =>
+  // eslint-disable-next-line react/no-render-return-value
+  ReactDOM.render(
+    <React.StrictMode>
       <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+if (process.env.NODE_ENV !== 'production') {
+  import('@axe-core/react').then((axe) => {
+    axe.default(React, ReactDOM, 1000);
+    render();
+  });
+} else {
+  render();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
